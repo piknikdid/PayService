b9c259413461046596cfa72ed9794f567cc53ef4
Nikita Pikarchuk <piknik68@gmail.com>
2017-10-29 23:08:34 +0200

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
-SQLALCHEMY_DATABASE_URI= 'mysql://root:admin123@localhost/invoice_data'
+SQLALCHEMY_DATABASE_URI= 'fill this field'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
@@ -12,5 +12,5 @@ SECRET_KEY = 'asdf86tasdhfuisdfKHgdfasuasdyf7asdf9'
 #PayService API params
 PS_TIP_URL = 'https://tip.pay-trio.com/ru/'
 PS_INVOICE_URL = 'https://central.pay-trio.com/invoice'
-PS_SHOP_ID = 305220
-PS_SECRET_KEY = 'jwyKXMJIAK9Z6aPmLHkouUbLE7cDWxZNh'
+PS_SHOP_ID = 'fill this field'
+PS_SECRET_KEY = 'fill this field'
